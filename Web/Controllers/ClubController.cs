7fb95dacc3c8f0f32cc1c238ca30ae434a0afd56
George Prado <george.prado@outlook.com>
2017-05-26 16:41:29 -0400

    added scafalding for Club Model

---
+++ Web/Controllers/ClubController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SAC.Domain;
+using SAC.Domain.Models;
+
+namespace SAC.Web.Controllers
+{
+    [Authorize]
+    [RequireHttps]
+    public class ClubController : Controller
+    {
+        private SacContext db = new SacContext();
+
+        // GET: Club
+        public ActionResult Index()
+        {
+            return View(db.Clubs.ToList());
+        }
+
+        // GET: Club/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Club club = db.Clubs.Find(id);
+            if (club == null)
+            {
+                return HttpNotFound();
+            }
+            return View(club);
+        }
+
+        // GET: Club/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Club/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Address,CityStateZip,Contact,Phone,Email,Website,Directions,IconFileName")] Club club)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Clubs.Add(club);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(club);
+        }
+
+        // GET: Club/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Club club = db.Clubs.Find(id);
+            if (club == null)
+            {
+                return HttpNotFound();
+            }
+            return View(club);
+        }
+
+        // POST: Club/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Address,CityStateZip,Contact,Phone,Email,Website,Directions,IconFileName")] Club club)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(club).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(club);
+        }
+
+        // GET: Club/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Club club = db.Clubs.Find(id);
+            if (club == null)
+            {
+                return HttpNotFound();
+            }
+            return View(club);
+        }
+
+        // POST: Club/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Club club = db.Clubs.Find(id);
+            db.Clubs.Remove(club);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
