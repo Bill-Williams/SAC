c7657b261461574c82ea00d742765d3f216d63de
George Prado <george.prado@outlook.com>
2017-06-19 14:35:02 -0400

    Removed unused using statements

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,19 +1,9 @@
-﻿using SAC.Web.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using SAC.Domain;
-using SAC.Domain.Models;
-using System.Data.Entity;
+﻿using System.Web.Mvc;
 
 namespace SAC.Web.Controllers
 {
     public class HomeController : Controller
     {
-        //private SacContext db = new SacContext();
-
         public ActionResult Index()
         {
             return View();
